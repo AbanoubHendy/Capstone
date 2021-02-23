8dbee6cb0b84c25b4beb0d355ed09ad4c59a547d
Abanoub Hendy <abanoubhendi@gmail.com>
2021-02-23 20:17:02 +0200

    second commit

--- src/Client/index.js
+++ src/Client/index.js
@@ -1,7 +1,1 @@
-import './Styles/base.scss'
-import './Styles/footer.scss'
-import './Styles/form.scss'
-import './Styles/header.scss'
-import './Styles/resets.scss'
-
 alert('I exist')
--- src/Server/server.js
+++ src/Server/server.js
@@ -1,17 +1,19 @@
 const bodyParser = require('body-parser')
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const webpack = require('webpack');
 const app = express()
 projectData ={};
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.static('src/Client'))
 app.get('/', function(req , res) {
     res.sendFile('./dist/index.html')
 })
-
-app.listen(7000 , function() {
-    console.log('Your App Running on Port 7000')
-})
+const port = 7000;
+const server = app.listen(port , listening);
+function listening() {
+    console.log(`the server running on: ${port}`);
+};
 
